c7d359df9f5ba357e3698399c8b8c7d8cf3dee44
Andrew <andreyshalagin88@yandex.ru>
2021-09-30 19:26:29 +0300

    movie research

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-confirm("Click it!");
+const numberOfFilms = +prompt("Сколько фильмов вы посмотрели на английском языке?", "");
+
+const lastMovie = prompt("Один из последних просмотренных фильмов", "");
+const raiting = prompt("На сколько вы оцениваете этот фильм?", "");
+
+const lastMovie1 = prompt("Один из последних просмотренных фильмов", "");
+const raiting1 = prompt("На сколько вы оцениваете этот фильм?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+personalMovieDB.movies[lastMovie] = raiting;
+personalMovieDB.movies[lastMovie1] = raiting1;
+
+console.log(personalMovieDB);
